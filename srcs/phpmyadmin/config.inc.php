88864c42fbc1d940b1cad890994aecd3ca595478
Tim van Citters <timvancitters@Tims-MacBook-Pro.local>
2020-11-17 13:02:01 +0100

    adjusted config.inc.php file phpmyadmin

--- srcs/phpmyadmin/config.inc.php
+++ srcs/phpmyadmin/config.inc.php
@@ -8,6 +8,7 @@ $i = 1;
 
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
+
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['compress'] = false;
